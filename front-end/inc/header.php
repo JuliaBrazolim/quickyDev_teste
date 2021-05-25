f853735b88418676f6693bf03ae9ead12d33fbe6
JuliaBrazolim <juliabrazolim@gmail.com>
2021-05-25 18:10:50 -0300

    campo de pesquisa

--- front-end/inc/header.php
+++ front-end/inc/header.php
@@ -8,7 +8,7 @@
   <link rel="stylesheet" href="<?php echo BASEURL ?>../css/style.css">
   </head>
   <body>
-    <header class="masthead">
+    <header class="masthead my-4">
       <div class="container h-100">
         <div class="row h-100 align-items-center">
           <div class="col-12 text-center">
@@ -16,6 +16,13 @@
             <h1 class="font-weight-light">Guia do Cinéfilo</h1>
             <p class="lead">Procure por filmes e seus detalhes!</p>
           </div>
+          <form class="form-inline mx-auto d-block">
+            <input class="form-control mr-sm-2" type="search" placeholder="Digite o título, gênero..." aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Procurar</button>
+          </form>
         </div>
       </div>
     </header>
+
+
+
