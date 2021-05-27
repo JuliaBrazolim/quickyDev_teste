6acbebb6fcd2238871faa81577d6089baa7bbf17
Julia Brazolim <juliabrazolim@gmail.com>
2021-05-26 22:57:49 -0300

    update

--- back-end/tmdb.js
+++ back-end/tmdb.js
@@ -7,7 +7,7 @@ $(document).ready(() => {
 });
 
 function getFilms(searchTitle) {
-    axios.get('https://api.themoviedb.org/3/search/movie?api_key=4ec327e462149c3710d63be84b81cf4f&language=en-US&query=' + searchTitle)
+    axios.get('https://api.themoviedb.org/3/search/movie?api_key=4ec327e462149c3710d63be84b81cf4f&language=pt-BR&query=' + searchTitle)
     .then(function (response) {
         let movies = response.data.results;
         let output = '';
@@ -15,14 +15,18 @@ function getFilms(searchTitle) {
             output += `
             <div class="col-md-4">
               <!-- resultado -->
-              <div class="card card-films mb-4 box-shadow">
+              
+              <div class="card  mb-3 box-shadow">
                 <img class="card-img-top" src="https://image.tmdb.org/t/p/w500${movie.poster_path}">
                     <div class="card-body">
                     <a class="link-cosmos">
-                    <h5 class="card-title text-left">${movie.title}</h5>
+                     <h5 class="card-title text-left">${movie.title}</h5>
                     </a>
+                    <p class="card-text text-justify">${movie.overview}</p>
+                    <p class="card-text text-justify">Lançado em: ${movie.release_date}</p>
+                     <small class="text-success">${movie.genres}</small>
                     </div>
-                    <a class="btn btn-outline-warning" onclick="movieSelected('$(movie.id)')">Saiba mais!</a>
+                    <a href="https://www.themoviedb.org/movie/${movie.id}" target="_blank" class="btn btn-primary">Saiba mais</a>
                 </div>
           </div>
             `;
@@ -34,3 +38,10 @@ function getFilms(searchTitle) {
         console.log(err);
     });
 }
+
+//Detalhes do filme
+//function filmSelected(id) {
+   // sessionStorage.setItem('movieId', id);
+    //window.location = 'details.php';
+    //return false;
+//}
