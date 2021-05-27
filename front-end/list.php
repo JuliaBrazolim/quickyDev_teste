829b9b9a32658fb0a525ef1a62d0e89101b97ca0
Julia Brazolim <juliabrazolim@gmail.com>
2021-05-26 22:52:53 -0300

    update

--- front-end/list.php
+++ front-end/list.php
@@ -2,9 +2,13 @@
 <?php include(HEADER_TEMPLATE); ?>
 <div class="container">
 
-<h4 class="text-center mt-4">Resultados da pesquisa:</h4>
-    <div class="album row py-5" id="movies">
-      
-    </div>
+<section class="mx-auto d-block text-center my-4">
+<form id="searchMovie" class="form-inline mr-5 mx-auto d-block">
+  <h6 class="text-center pb-3">Digite um título ou gênero de filme e dê <i>enter</i>:</h6>
+    <input class="form-control" type="text" id="searchTitle" placeholder="Exemplo: Hulk" aria-label="Search">
+</form>
+</section>
+
+  <div class="album row py-5" id="movies"></div>
 </div>
 <?php include(FOOTER_TEMPLATE); ?>
