6a7ab59c8b3f028503aa0a0ff72b68266a24c9bc
Julia Brazolim <juliabrazolim@gmail.com>
2021-05-26 22:53:29 -0300

    vazio

---
+++ front-end/details.php
@@ -0,0 +1,14 @@
+<?php require_once 'config.php'; ?>
+<?php include(HEADER_TEMPLATE); ?>
+<div class="container">
+
+<h4 class="text-center mt-4">Detalhes do filme:</h4>
+    <div class="album row py-5" id="details_movie">
+      
+    </div>
+</div>
+
+<script>
+    getFilms();
+</script>
+<?php include(FOOTER_TEMPLATE); ?>
