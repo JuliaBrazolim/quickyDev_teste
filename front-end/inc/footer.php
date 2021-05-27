e3881091fc9aba1996fb0aefcf1a955b31d793d0
Julia Brazolim <juliabrazolim@gmail.com>
2021-05-26 22:54:56 -0300

    update

--- front-end/inc/footer.php
+++ front-end/inc/footer.php
@@ -1,8 +1,6 @@
 <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
 <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
 <script src="../back-end/tmdb.js"></script>
-<footer>
-    <small>Test by Julia Brazolim</small>
-</footer>
+
 </body>
 </html>
